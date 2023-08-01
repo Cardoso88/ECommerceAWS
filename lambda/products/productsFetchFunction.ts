52b2e0e8697c8308d934371924d6c35f1894ff38
Cardoso88 <alexmcardoso@gmail.com>
2023-08-01 14:11:33 -0300

    add handler stack product

---
+++ lambda/products/productsFetchFunction.ts
@@ -0,0 +1,26 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+
+export async function handler (event: APIGatewayProxyEvent,
+  context: Context): Promise<APIGatewayProxyResult> {
+  
+    const lambdaRequestId = context.awsRequestId
+    const apiRequestID = event.requestContext.requestId
+
+    console.log(`API Gateway RequestId: ${apiRequestID} - Lambda RequestId: ${lambdaRequestId}`)
+    
+    const method = event.httpMethod
+    if (event.resource === "/products") {
+      if (method === 'GET'){
+        console.log('GET')
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: "GET Products - OK" })
+        }
+      }
+    }
+
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Bad request" })
+    }
+}
