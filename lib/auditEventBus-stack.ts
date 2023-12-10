031a0781f9f3b0e137fb33cc13225b3c78460c27
Cardoso88 <alexmcardoso@gmail.com>
2023-12-10 18:13:43 -0300

    add alarm Invoice Import Timeout

--- lib/auditEventBus-stack.ts
+++ lib/auditEventBus-stack.ts
@@ -5,6 +5,7 @@ import * as sqs from "aws-cdk-lib/aws-sqs"
 import * as events from "aws-cdk-lib/aws-events"
 import * as targets from "aws-cdk-lib/aws-events-targets"
 import { Construct } from 'constructs'
+import * as cw from "aws-cdk-lib/aws-cloudwatch"
 
 export class AuditEventBusStack extends cdk.Stack {
   readonly bus: events.EventBus
@@ -107,5 +108,22 @@ export class AuditEventBusStack extends cdk.Stack {
       queueName: 'invoice-import-timeout'
     })
     timeoutImportInvoiceRule.addTarget(new targets.SqsQueue(invoiceImportTimeoutQueue))
+
+    //Metric
+    const numberOfMessagesMetric = invoiceImportTimeoutQueue.metricApproximateNumberOfMessagesVisible({
+      period: cdk.Duration.minutes(2),
+      statistic: "Sum"
+    })
+    
+    //Alarm
+    numberOfMessagesMetric.createAlarm(this, "InvoiceImportTimeoutAlarm", {
+      alarmName: "InvoiceImportTimeout",
+      actionsEnabled: false,
+      evaluationPeriods: 1,
+      threshold: 5,
+      comparisonOperator: cw.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD
+    })
+
+    
   }
 }
